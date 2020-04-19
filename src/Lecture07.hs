de6a7a234a547b259f397ed33916caa6f20b58be
A.Voi <voytsishevskiy@tochka.com>
2020-04-19 19:16:43 +0500

    solve hw7

--- src/Lecture07.hs
+++ src/Lecture07.hs
@@ -1,11 +1,13 @@
-{-# OPTIONS_GHC -fno-warn-orphans -fno-warn-unused-imports #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE KindSignatures #-}
+{-# OPTIONS_GHC -fno-warn-orphans -fno-warn-unused-imports #-}
+
 module Lecture07 where
 
-import Lecture07.Money
-import Data.Semigroup
 import Data.Foldable
+import Data.Semigroup
+import Lecture07.Money
 
 {-
   07: Классы типов
@@ -41,7 +43,7 @@ import Data.Foldable
       - UndecidableInstances
       - OverlappingInstances
       - IncoherentInstances
-      - ConstrainedClassMethods 
+      - ConstrainedClassMethods
   - deriving (Eq, Show)
     - https://gitlab.haskell.org/ghc/ghc/-/wikis/commentary/compiler/generic-deriving
 -}
@@ -63,17 +65,40 @@ data Expr
   | Mult Expr Expr
   | UnaryMinus Expr
   | Abs Expr
-  deriving Eq
+  deriving (Eq)
 
+instance Show Expr where
+  show n = let 
+        showB (Number i) = show i
+        showB m = "(" ++ show m ++ ")" 
+        in 
+          case n of
+   (Plus x y) -> showB x ++ " + " ++ showB y 
+   (Minus x y) -> showB x ++ " - " ++ showB y
+   (Mult x y) ->  showB x ++ " * " ++ showB y
+   (Abs e) -> "|" ++ show e ++ "|"
+   (UnaryMinus i) -> "-" ++ showB i
+   _ -> showB n
+      
+    
 {-
   Реализуйте instance Semigroup для вектора:
 -}
-newtype Vec a = Vec { unVec :: [a] } deriving (Eq, Show)
+newtype Vec a = Vec {unVec :: [a]} deriving (Eq, Show)
+
+instance Num a => Semigroup (Vec a) where 
+  Vec as <> Vec bs = Vec $ map s . zip as $ bs
+                          where 
+                            s (a, b) = a + b
+
 
 {-
   Реализуйте instance Semigroup для типа для логгирования:
 -}
-newtype LogEntry = LogEntry { unLogEntry :: String } deriving (Eq, Show)
+newtype LogEntry = LogEntry {unLogEntry :: String} deriving (Eq, Show)
+
+instance Semigroup LogEntry where 
+  LogEntry a <> LogEntry b =  LogEntry $ a ++ b
 
 {-
   В `src/Lecture07/Money.hs` определены:
@@ -83,21 +108,35 @@ newtype LogEntry = LogEntry { unLogEntry :: String } deriving (Eq, Show)
 
   Реализуйте инстансы Semigroup для Money a.
 -}
+instance Semigroup (Money RUB) where 
+  ma <> mb = mkRubbles $ (getMoney ma) + (getMoney mb)
+
+instance Semigroup (Money USD) where 
+  ma <> mb = mkDollars $ (getMoney ma) + (getMoney mb)
 
 {-
   Реализуйте инстанс Functor для ExactlyOne
 -}
 data ExactlyOne a = ExactlyOne a deriving (Eq, Show)
 
+instance Functor ExactlyOne where 
+  fmap f (ExactlyOne a) = ExactlyOne $ f a
 {-
   Реализуйте инстанс Functor для `Maybe a`
 -}
 data Maybe' a = Just' a | Nothing' deriving (Eq, Show)
 
+instance Functor Maybe' where 
+  fmap f (Just' a) = Just' $ f a
+  fmap _ _ = Nothing'
 {-
   Реализуйте инстанс Functor для `List a`
 -}
 data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+instance Functor List where 
+  fmap _ Nil = Nil
+  fmap f (Cons x xs) = f x `Cons` fmap f xs
 
 {-
   `FileTree a` — тип для представления дерева файловой системы.
@@ -111,12 +150,14 @@ data FileTree a
   deriving (Eq, Show)
 
 {-
-  `FileInfo` — тип с информацией о файле: содержит его размер и дату последнего изменения. 
+  `FileInfo` — тип с информацией о файле: содержит его размер и дату последнего изменения.
 -}
-data FileInfo = FileInfo
-  { size :: Integer
-  , modified :: String
-  } deriving (Eq, Show)
+data FileInfo
+  = FileInfo
+      { size :: Integer,
+        modified :: String
+      }
+  deriving (Eq, Show)
 
 {-
   Тогда мы можем строить деревья типа `FileTree FileInfo` и работать с ними.
@@ -127,7 +168,8 @@ data FileInfo = FileInfo
 
 -- Пример использования Foldable для суммирования размера файла
 fileSizeOfTree :: FileTree FileInfo -> Integer
-fileSizeOfTree = getSum . foldMap (\FileInfo{..} -> Sum size)
+fileSizeOfTree = getSum . foldMap (\FileInfo {..} -> Sum size)
+
 -- С помощью расширения RecordWildCards     ^
 -- мы раскрываем record и получаем доступ к полю size   ^^^^
 
@@ -138,15 +180,23 @@ instance Bounded [Char] where
 
 -- А здесь используется для нахождения даты последнего изменения
 latestModified :: FileTree FileInfo -> String
-latestModified = getMax . foldMap (\FileInfo{..} -> Max modified)
+latestModified = getMax . foldMap (\FileInfo {..} -> Max modified)
 
 {-
   Чтобы функции выше работали, необходимо
   реализовать instance Foldable для FileTree:
 -}
+-- data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a)
+
+-- instance Foldable Tree where
+--    foldMap f Empty = mempty
+--    foldMap f (Leaf x) = f x
+--    foldMap f (Node l k r) = foldMap f l `mappend` f k `mappend` foldMap f r
 
 instance Foldable FileTree where
-  foldMap = undefined
+  foldMap _ Empty = mempty
+  foldMap g (File _ a) = g a
+  foldMap g (Dir _ fa) = foldMap (foldMap g) fa
 
 {-
   В этом задании вам необходимо придумать и написать иерархию исключений
@@ -159,9 +209,29 @@ instance Foldable FileTree where
   3. Класс для ошибок при работе с базой данных.
     - дополнительно возвращает сообщение об ошибке от базы данных
   4. Класс для ошибок доменной логики
-    - дополнительно возвращает контекс с данными
+    - дополнительно возвращает контекст с данными
 
   Реализовывать инстансы не нужно.
 -}
+newtype Json = Json String
+data Severity = Debug | Info | Error | Warn
+newtype ErrorMessage = ErrorMessage String
+newtype DatabaseErrorMessage = DatabaseErrorMessage String
+
+class Exception (m :: * -> *) where
+  getMessage :: m a -> ErrorMessage
+
+class Exception m => ApiException (m :: * -> *) where
+  toJson :: m a -> Json
+  getSeverity :: m a -> Severity
+
+class Exception m => DatabaseException (m :: * -> *) where
+  getDatabaseError :: m a -> DatabaseErrorMessage
+
+class Exception m => DomainException (m :: * -> *) where
+  getContextfulMessage :: m a -> (ErrorMessage, a)
+
+
+
 
 -- </Задачи для самостоятельного решения>
